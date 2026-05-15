e685b4e45f40c01bcf511185c5f2d08c46b4a89c
quyetdeveloper <quyetbkhoa@gmail.com>
2026-05-15 16:21:10 +0700

    remove fingerprint ui

--- bin/package/Universal/Fingerprint/update.sh
+++
@@ -1,5 +0,0 @@
-work_dir=$(pwd)
-source $work_dir/functions.sh
-MAIN_FOLDER="$work_dir/build/baserom/images"
-
-cp -rf $work_dir/bin/package/Universal/Fingerprint/overlay/* $MAIN_FOLDER/my_product/overlay/
